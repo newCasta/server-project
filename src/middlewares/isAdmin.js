154449044c48fd82f9677e1fde12f830f9f868be
siCasta <sicastaprojectos@gmail.com>
2022-11-07 19:18:03 -0500

    some changes

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -3,7 +3,7 @@ import createError from 'http-errors'
 export const isAdmin = (req, res, next) => {
     if (!req.session.user) return next(createError(401, 'You are not logged'))
 
-    const isAdmin = req.session.user.permissions.include('admin')
+    const isAdmin = req.session.user.permissions.includes('admin')
 
     if (isAdmin) return next()
 
